11cee4b8f56f421ea1926dfc6ea3f1c86c1986fd
Hemant Mann <hemant.mann121@gmail.com>
2016-06-18 16:09:29 +0530

    Removed Caching

--- app.js
+++ app.js
@@ -22,7 +22,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+    maxAge: 0,
+    etag: false,
+    setHeaders: function (res, path) {
+        res.setHeader('Cache-Control', 'no-cache, max-age=0')
+    }
+}));
 
 app.use('/', routes);
 
